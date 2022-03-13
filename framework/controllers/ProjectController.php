e68f2f4ac716f658cd3316a1c8ab05d904d7cdea
Calil Christopher Jaudian <calilchristopher052997@gmail.com>
2022-03-13 11:41:53 +0800

    created project model and controller

---
+++ framework/controllers/ProjectController.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once '../models/ProjectModel.php';
+include_once '../../autoload.php';
+
+$act = isset($_GET['action']) ? $_GET['action'] : '';
+
+switch ($act) {
+    case 'create_project':
+        $new_project = $project_model->create_project($_POST);
+
+        echo json_encode([
+            "success"   => $new_project > 0 ? 1 : 0,
+            "insert_id" => $new_project,
+            "action"    => "ProjectController/action=create_project"
+        ]);
+        exit;
+        break;
+
+    case 'update_project':
+        $new_project = $project_model->update_project($_POST);
+
+        echo json_encode([
+            "success"    => intval($new_project) > 0 ? 1 : 0,
+            "updated_id" => $new_project,
+            "action"     => "ProjectController/action=update_project"
+        ]);
+        exit;
+
+    case 'get_projects':
+        echo json_encode([
+            "data"       =>  $project_model->get_projects($_GET['user_id']),
+            "success"    => 1,
+            "action"     => "ProjectController/action=get_projects"
+        ]);
+        exit;
+        break;
+    
+    default:
+        # code...
+        break;
+}
---
+++ framework/models/ProjectModel.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once '../../autoload.php';
+
+class ProjectModel {
+
+    private $base_table = 'ce_projects';
+
+    public function create_project($payload = [])
+    {
+        global $db, $common;
+
+        $arr = [
+            "name"         => $payload['name'],
+            "description"  => $payload['description'],
+            "type"         => $payload['type'],
+            "created_by"   => $payload['created_by'],
+            "date_created" => date('Y-m-d H:i:s'),
+            "sq_meters"    => $payload['sq_meters'],
+        ];
+
+        $fields = $common->get_insert_fields($arr);
+
+        return $db->query("INSERT INTO {$this->base_table} {$fields} VALUES (?,?,?,?,?,?)", array_values($arr));
+    }
+
+    public function update_project($payload = [])
+    {
+        global $db, $common;
+
+        $project_pk = $payload['project_id'];
+
+        unset($payload['project_id']);
+        $fields = $common->get_update_fields($payload);
+
+        $payload['project_id'] = $project_pk;
+        $db->query("UPDATE {$this->base_table} SET {$fields} WHERE id = ?", array_values($payload));
+
+        return $project_pk;
+    }
+    
+    public function get_projects($id) 
+    {
+        global $db, $common;
+
+        return $db->select("SELECT * FROM {$this->base_table} WHERE created_by = ?", [$id]);
+    }
+
+}
+
+$project_model = new ProjectModel();
